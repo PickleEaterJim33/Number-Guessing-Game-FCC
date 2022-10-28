a7b361edee76de177d3ec6da23e104f2366785b4
PickleEaterJim33 <jan.mlinaric@gmail.com>
2022-10-28 19:07:16 +0000

    feat: add necessary columns

--- number_guess.sql
+++ number_guess.sql
@@ -49,7 +49,9 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.users (
     user_id integer NOT NULL,
-    username character varying(22) NOT NULL
+    username character varying(22) NOT NULL,
+    games_played integer DEFAULT 0,
+    best_game integer DEFAULT '-1'::integer
 );
 
 
